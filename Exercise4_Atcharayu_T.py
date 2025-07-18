020f8b399b3dda608b56c7a594f8a37a540b27e1
Atcharayu Tunyakan <147295472+R-7wX@users.noreply.github.com>
2025-07-18 17:18:07 +0700

    Added Exercise 4

---
+++ Exercise4_Atcharayu_T.py
@@ -0,0 +1,11 @@
+#Input Section
+Foundation_English=float(input("Foundation English : "))
+General_Business =float(input("General Business : "))
+Introduction_to_Computer_Systems = float(input("Introduction to Computer Systems : "))
+Computer_Programming =float(input("Computer Programming  : "))
+#Output Section
+print("--- Your Score ---")
+print("Foundation English : ",Foundation_English)
+print("General Business : ",General_Business)
+print("Introduction to Computer Systems : ",Introduction_to_Computer_Systems)
+print("Computer Programming : ",Computer_Programming)
